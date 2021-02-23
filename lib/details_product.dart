1a54fdd98c4db081cd02faab6c4070f94b446d95
FlutLab.io <>
2021-02-23 17:58:31 +0000

    Autosave

---
+++ lib/details_product.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget{}
+  final int index;
+
+  DetailPage(this.index);
+
+  override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Какое-то описание товара'),
+      ),
+      body: Center(
+        child: Text('Какой-то текс описания товара')
+      )
+    );
+  }
+}
