2e31f65a56fb07a1c54c6fb91e6906eb16e59d77
FlutLab.io <>
2021-02-23 18:35:51 +0000

    Autosave

--- lib/details_product.dart
+++ lib/details_product.dart
@@ -12,7 +12,7 @@ class DetailPage extends StatelessWidget{
         title: Text('Какое-то описание товара'),
       ),
       body: Center(
-        child: Text('Какой-то текс описания товара'),
+        child: Text('Какой-то текс описания товара #$index'),
       ),
     );
   }
